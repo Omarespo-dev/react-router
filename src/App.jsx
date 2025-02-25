1ecbf79286d71abdc4e8bb43f910b09b9b3a6fe8
Omarespo-dev <omaresposito21@gmail.com>
2025-02-25 17:51:59 +0100

    Added Redirect and Error 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import ChiSiamoPages from "./pages/ChiSiamoPages";
 import ListPostsPages from "./pages/ListPostsPages";
 import CreatePostPage from "./pages/CreatePostPage";
 import PostsDetails from "./pages/PostsDetails";
+import NotFoundPage from "./pages/NotFoundPage";
 // Importo I componenti del router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , Navigate} from "react-router-dom";
 
 // Layout andiamo a richiamare a riga 23
 import DefaultLayout from "./layouts/DefaultLayout";
@@ -27,6 +28,10 @@ function App() {
           <Route path="/listposts"element={<ListPostsPages />} />
           <Route path="/createposts"element={<CreatePostPage />} />
           <Route path="/listposts/:id" element={<PostsDetails />} />
+          {/* REDIRECT */}
+          <Route path="/listapost" element={<Navigate to="/listposts" />} />
+          {/* rotta del 404 per rotte non previste */}
+          <Route path="*" element={<NotFoundPage />} />
           
           {/* <Route path='/listposts'>
             <Route index element={<ListPostsPages />} />
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+export default function NotFoundPage() {
+
+    return (
+        <>
+            <h1>Pagina non trovata</h1>
+            
+            <p>
+                404 la pagine che cerchi non è stata trovata
+            </p>
+
+        </>
+    );
+}
